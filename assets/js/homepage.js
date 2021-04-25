b2a59df96266db1e9e65ad6ad82ebdb9795b78f7
Lexie Crawford <lexcraw4d@gmail.com>
2021-04-25 19:28:09 -0400

    successfully search via console with fetch method using user input val

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,34 @@
-var getUserRepos = function(user) {
-  // format the github api url
-  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username');
 
-  // make a request to the url
-  fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-      console.log(data);
-    });
-  });
+var formSubmitHandler = function (event) {
+	// prevent page from refreshing
+	event.preventDefault();
+
+	// get value from input element
+	var username = nameInputEl.value.trim();
+
+	if (username) {
+		getUserRepos(username);
+
+		nameInputEl.value = '';
+	} else {
+		alert('Please enter a GitHub username');
+	}
 };
-  getUserRepos("lexcraw4d")
+
+var getUserRepos = function (user) {
+	// format the github api url
+	var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+	// make a request to the url
+	fetch(apiUrl).then(function (response) {
+		response.json().then(function (data) {
+			console.log(data);
+		});
+	});
+};
+//Test to see if fetch works properly
+// getUserRepos("lexcraw4d");
+
+userFormEl.addEventListener('submit', formSubmitHandler);
